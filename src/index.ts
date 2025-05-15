9a0991378034c30f269c494afa4482c532186364
bi3mer <colan@comcast.net>
2025-05-15 18:13:22 -0400

    removed button behavior which was causing an error

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,16 @@ import { fischerYatesShuffle } from "./Random";
 
 DB.init();
 
-// -------------- Set up player id --------------- 
-if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === '') {
+// -------------- Set up player id ---------------
+if (
+  location.hostname === "localhost" ||
+  location.hostname === "127.0.0.1" ||
+  location.hostname === ""
+) {
   Global.playerID = "-1";
 } else {
   const urlParams = new URLSearchParams(window.location.search);
-  if (urlParams.has('id')) {
+  if (urlParams.has("id")) {
     Global.playerID = crypto.randomUUID();
   } else {
     Global.playerID = "-1";
@@ -21,7 +25,7 @@ if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || lo
 
 console.log(`Player ID: ${Global.playerID}`);
 
-// -------------- Set up the engine -------------- 
+// -------------- Set up the engine --------------
 const engine = new Engine();
 engine.displayFPS = false;
 
@@ -36,7 +40,7 @@ const startIndex = engine.addScene(startScene);
 const gameIndex = engine.addScene(gameScene);
 const lostIndex = engine.addScene(playerLostScene);
 const wonIndex = engine.addScene(playerWonScene);
-const tutorialIndex = engine.addScene(tutorialScene)
+const tutorialIndex = engine.addScene(tutorialScene);
 const surveyIndex = engine.addScene(surveyScene);
 
 startScene.tutorialIndex = tutorialIndex;
@@ -67,7 +71,7 @@ const questionnaire = document.getElementById("questionnaire");
 let i = 0;
 for (; i < QUESTIONS.length; ++i) {
   const q = QUESTIONS[i];
-  const id = q.split(' ').join('_');
+  const id = q.split(" ").join("_");
 
   const formElement = `<fieldset id="${id}">
   <label for="${id}" required><b>${q}</b></label>
@@ -104,9 +108,9 @@ for (; i < QUESTIONS.length; ++i) {
 }
 
 // create the submit button
-var submitButton = document.createElement('button');
-submitButton.type = 'submit';
-submitButton.innerText = 'Submit';
+var submitButton = document.createElement("button");
+submitButton.type = "submit";
+submitButton.innerText = "Submit";
 questionnaire!.appendChild(submitButton);
 
 // form submission behavior
@@ -117,11 +121,12 @@ questionnaire!.onsubmit = (event) => {
 
   for (i = 0; i < QUESTIONS.length; ++i) {
     const Q = QUESTIONS[i];
-    const N = Q.split(' ').join('_')
+    const N = Q.split(" ").join("_");
     let elements = document.getElementsByName(N);
 
     let found = false;
-    for (let j = 0; j < 7; ++j) { // size will always be 7, look at radio buttons above
+    for (let j = 0; j < 7; ++j) {
+      // size will always be 7, look at radio buttons above
       const E = elements[j] as HTMLInputElement;
       if (E.checked) {
         answers[Q] = Number(E.value);
@@ -130,23 +135,16 @@ questionnaire!.onsubmit = (event) => {
       }
     }
 
-    document.getElementById(N)!.style.borderColor = found ? 'white' : 'red';
+    document.getElementById(N)!.style.borderColor = found ? "white" : "red";
   }
 
   if (Object.keys(answers).length == QUESTIONS.length) {
     DB.submitSurvey(answers);
-    console.log('Survey submitted');
-    document.getElementById('survey')!.style.display = "none";
-    document.getElementById('complete')!.style.display = "block";
+    console.log("Survey submitted");
+    document.getElementById("survey")!.style.display = "none";
+    document.getElementById("complete")!.style.display = "block";
   } else {
-    document.getElementById('errorText')!.innerText = 'Please fill in the whole questionnaire. Red boxes indicate missed answers.';
+    document.getElementById("errorText")!.innerText =
+      "Please fill in the whole questionnaire. Red boxes indicate missed answers.";
   }
-};
-
-// -------------- Button Behavior --------------- 
-document.getElementById('done')!.onclick = () => {
-  engine.shutoff();
-
-  document.getElementById('game')!.style.display = "none";
-  document.getElementById('survey')!.style.display = "block";
 };
