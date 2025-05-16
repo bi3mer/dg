d5b1c4c2f05d7f2d7fbc6482605ec090245d530c
bi3mer <colan@comcast.net>
2025-05-16 18:23:45 -0400

    added more level segments and wrote a script to test whether every edge in the graph was competable. This required adding the dungeongrams submodule

---
+++ levels/test_edges.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+from dungeongrams.dungeongrams import FLAW_NO_FLAW, percent_playable
+
+with open("graph.json", "r") as f:
+    graph = json.load(f)
+
+
+id_to_lvls = {}
+for src in graph['graph']:
+    if src == "start":
+        continue
+
+    with open(os.path.join("segments", f"{src}.txt")) as f:
+        levels = []
+        lvl = []
+        for line in f:
+            l = line.strip()
+            if l == "&":
+                levels.append(lvl)
+                lvl = []
+            else:
+                lvl.append(l)
+
+        levels.append(lvl)
+        id_to_lvls[src] = levels
+
+invalids = []
+for src_name, src_lvls in id_to_lvls.items():
+    print(src_name)
+    for tgt_name in graph['graph'][src_name]["neighbors"]:
+        for i_src, src_lvl in enumerate(src_lvls):
+            for i_tgt, tgt_lvl in enumerate(id_to_lvls[tgt_name]):
+                combined_level = [src_lvl[i] + tgt_lvl[i] for i in range(len(src_lvl))]
+                if percent_playable(combined_level, False, True, True, FLAW_NO_FLAW) != 1.0:
+                    invalids.append(f"{src_name}-{i_src} -> ${tgt_name}-{i_tgt} not playable.")
+
+if len(invalids) == 0:
+    print("Every edge was playable!")
+else:
+    for text in invalids:
+        print(text)
